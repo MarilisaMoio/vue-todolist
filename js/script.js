d281436c153e6a764540a21258899f506fea144d
Marilisa Moio <moiomarilisa@gmail.com>
2024-04-02 16:14:47 +0200

    feat: add function for object generation

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
+            task: "",
             todoTasks: [],
+        }
+    },
+    methods: {
+        addNewTask(){
+            const newActivity = {};
+            newActivity.task = this.task.trim();
+            newActivity.done = false;
+            this.todoTasks.push(newActivity)
         }
     }
 }).mount(app);
